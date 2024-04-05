8dbf014cdf06d37e0c3d46eaabe9628db9fe0906
Antonio Castles <info@antoniocastles.net>
2024-04-05 19:11:12 +0200

    corrected command.sh

--- command.sh
+++ command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ./subtitle -f fonts/6x13.bdf -C 255,255,255 -s -0 --led-slowdown-gpio=4 --led-cols=192 -x 8 -y 4 --led-brightness=50 --led-no-hardware-pulse -i ./input.txt
+sudo ./subtitle -f fonts/6x13.bdf -C 255,255,255 -s -0 --led-slowdown-gpio=4 --led-cols=64 --led-rows=32 --led-chain=4 --led-gpio-mapping=adafruit-hat -x 8 -y 4 --led-brightness=50 --led-no-hardware-pulse -i ./input.txt
