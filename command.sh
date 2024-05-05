8648c42b6a8488ee643a60c82ab9b2ae414095f3
Antonio Castles <info@antoniocastles.net>
2024-05-05 15:01:27 +0200

    fixing

--- command.sh
+++ command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ./subtitle -f fonts/6x13.bdf -C 255,255,255 -s -0 --led-slowdown-gpio=4 --led-cols=64 --led-rows=32 --led-chain=3 --led-row-addr-type=0 -x 8 -y 5 --led-brightness=50 --led-no-hardware-pulse -i ./input.txt
+sudo ./subtitle -f fonts/7x13B.bdf -C 255,255,255 -s -0 --led-slowdown-gpio=5 --led-cols=64 --led-rows=32 --led-chain=6 --led-row-addr-type=0 -x 50 -y 0 --led-brightness=100 --led-no-hardware-pulse --led-pixel-mapper "Rotate:180" -i ./input.txt
